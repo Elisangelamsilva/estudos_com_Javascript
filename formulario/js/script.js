a035c844c1a725e1720a83afcaf1785486fb4be9
Elisangela <elisangela00silva@gmail.com>
2020-04-17 15:16:30 -0300

    Inicio

---
+++ formulario/js/script.js
@@ -0,0 +1,11 @@
+// IIFE
+function meuEScopo () {
+    const form = document.querySelector('.form');
+
+    // evento de submissão quando o form for enviado
+    form.onsubmit = function (event) {
+
+    };
+}
+
+meuEScopo ();
