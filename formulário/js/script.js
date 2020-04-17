d9016a4162bed35ed9037b741b0a01cd37bb4897
Elisangela <elisangela00silva@gmail.com>
2020-04-17 15:39:30 -0300

    Finalização

---
+++ formulário/js/script.js
@@ -0,0 +1,25 @@
+// IIFE
+function meuEScopo () {
+    const form = document.querySelector('.form');
+    const resultado = document.querySelector('.resultado');
+
+    // // evento de submissão quando o form for enviado
+    // form.onsubmit = function (event) {
+    //     event.preventDefault();
+    // };
+
+    // Mesma coisa só que com outro método 
+
+    function recebeEventoForm (evento) {
+        evento.preventDefault();
+       const nome = form .querySelector('.nome');
+       const sobrenome = form .querySelector('.sobrenome');
+       const peso = form .querySelector('.peso');
+       const altura = form .querySelector('.altura');
+
+    }
+
+    form.addEventListener('submit', recebeEventoForm);
+}
+
+meuEScopo ();
