a4606dabf0eecb4a631c20dda7363b464bbc6375
Elisangela <elisangela00silva@gmail.com>
2020-04-21 07:21:34 -0300

    if (imc >= 39.9)

--- IMC_JavaScript/assets/js/main.js
+++ IMC_JavaScript/assets/js/main.js
@@ -25,6 +25,19 @@ form.addEventListener("submit", function (event){
 
 });
 
+    //Lógica para associar o resultado a uma mensagem
+    function getNivelImc (imc){
+        const nivel = ['Abaixo do peso', 'Peso normal', 'Sobrepeso', 
+    'Obesidade grau 1', 'Obesidade grau 2', 'Obesidade grau 3']; // pegar as mensagens
+
+    if (imc >= 39.9) return nivel[5];
+    if (imc >= 34.9) return nivel[4];
+    if (imc >= 29.9) return nivel[3];
+    if (imc >= 24.9) return nivel[2];
+    if (imc >= 18.5) return nivel[1];
+    if (imc < 18.5)  return nivel[0];
+    }
+
 function getImc (peso, altura) {
     const imc = peso / altura ** 2;
     return imc.toFixed(2);
