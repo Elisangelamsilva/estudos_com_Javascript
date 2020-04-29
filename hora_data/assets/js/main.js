9f221cd2b491046aa161295fee46bc44811b9f99
Elisangela <elisangela00silva@gmail.com>
2020-04-29 11:48:11 -0300

    Add opçao para dia, data e hora

--- hora_data/assets/js/main.js
+++ hora_data/assets/js/main.js
@@ -1,4 +1,9 @@
 const h1 = document.querySelector(".container h1");
 const data = new Date();
-h1.innerHTML = data.toString();
+const opcoes = {
+    dateStyle: "full",
+    timeStyle: "short"
+};
+
+h1.innerHTML = data.toLocaleDateString("pt-BR", opcoes);
 
