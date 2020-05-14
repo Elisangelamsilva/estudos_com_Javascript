fa933985588b82768bd503e5694bded89d739353
Elisangela <elisangela00silva@gmail.com>
2020-05-14 10:52:35 -0300

    auto-atribuição variação

--- JavaScript-Guanabara/aula03/variaveis.js
+++ JavaScript-Guanabara/aula03/variaveis.js
@@ -4,3 +4,13 @@ var c = 5 * b ** 2
 var d = 10 - a/2
 var e = 6*2/d
 var f = b % e + 4 / e
+
+// auto-atribuição
+
+var n = 3
+n = n + 4 // n+=4
+n = n - 5 // n-=5
+n = n * 4 // n*=4
+n = n/2   // n/=2
+m = n ** 2 // n**=2
+n = n % 5
