3b91f919289d17b70c3b99921f9796af843a8e3c
Elisangela <elisangela00silva@gmail.com>
2020-05-22 18:02:13 -0300

    finalizado

--- manipular DOM com js/exercicio2/script.js
+++ manipular DOM com js/exercicio2/script.js
@@ -1,9 +1,10 @@
 function fatoriar() {
-    var rece2 = window.document.querySelector("input#recebeu")
+    var rece2 = window.document.querySelector("input#recebeu").value
+    var resultado = 1
 
-    for(var i = 1; i < resultado; i++){
-        resultado = rece2 * i;
+    for(var i = rece2; i > 1; i--){
+        resultado = resultado * i;
     }
 
-    resultado.innerHTML = `A fatoração do número ${rece2} é <strong>${resultado}</strong>`
+    res.innerHTML = `A fatoração do número ${rece2}! é <strong>${resultado}</strong>`
 }
