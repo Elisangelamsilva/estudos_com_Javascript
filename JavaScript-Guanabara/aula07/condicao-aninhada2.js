f54a0c2c3155f4d2d000a7bbea634101c275616b
Elisangela <elisangela00silva@gmail.com>
2020-05-27 11:40:18 -0300

    atualização da lógica do site condição de horas aninhadas

---
+++ JavaScript-Guanabara/aula07/condicao-aninhada2.js
@@ -0,0 +1,11 @@
+var hora = 12
+console.log(`Agora são exatamente ${hora} horas.`)
+if (hora >= 5 & hora <= 12){
+    console.log(`Bom dia!`)
+} else if (hora > 12 & hora < 18){
+    console.log("Boa tarde!")
+}if (hora <= 18 & hora >= 23) { 
+    console.log ('Boa noite!')
+} if (hora >= 1 & hora <= 4) {
+    console.log("Boa madrugada!")
+}
