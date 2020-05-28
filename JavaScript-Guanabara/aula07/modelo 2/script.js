7bbe85323e6e46e639f13748db31c9cb287e7e81
Elisangela <elisangela00silva@gmail.com>
2020-05-28 19:50:07 -0300

    mostrar img no html depois do onclick

--- JavaScript-Guanabara/aula07/modelo 2/script.js
+++ JavaScript-Guanabara/aula07/modelo 2/script.js
@@ -47,6 +47,7 @@ function verificar(){
         }
         res.style.textAlign = 'center'
         res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+        res.appendChild(img)
         
     }
 }
