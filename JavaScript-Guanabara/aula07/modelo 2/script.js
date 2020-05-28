b245e766fb83aa47d8f571843b2ca60401030439
Elisangela <elisangela00silva@gmail.com>
2020-05-28 18:58:17 -0300

    lógica caso seja colocado ano errado

--- JavaScript-Guanabara/aula07/modelo 2/script.js
+++ JavaScript-Guanabara/aula07/modelo 2/script.js
@@ -3,4 +3,8 @@ function verificar(){
     var ano = data.getFullYear() // ano atual com 4 casas
     var fano = document.getElementById("txtano")
     var res = document.getElementById('res')
+
+    if(fano.nodeValue.length == 0 || fano.value > 0){
+        window.alert("[ERRO] Verifique os dados e tente novamente!")
+    }
 }
