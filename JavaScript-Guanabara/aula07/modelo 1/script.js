91f5f877dc301d1f1cb42022c5511655aad9cbc1
Elisangela <elisangela00silva@gmail.com>
2020-05-28 13:11:40 -0300

    atualização lógica

--- JavaScript-Guanabara/aula07/modelo 1/script.js
+++ JavaScript-Guanabara/aula07/modelo 1/script.js
@@ -1,21 +1,21 @@
 function carregar(){
-    var msg = window.document.querySelector('div#msg')
-    var img = window.document.querySelector('div#img1')
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
     var data = new Date()
     var hora = data.getHours()
     msg.innerHTML = `Agora são ${hora} horas.`
 
-    if (hora >= 5 & hora <= 12){
+    if (hora >= 5 && hora <= 12){
         // BOM DIA
-        img.src = "foto-manha.jpg."
-    } else if (hora > 12 & hora < 18){
+        img.src = "img/foto-manha.jpg"
+    } else if (hora > 12 && hora < 18){
         // BOA TARDE!
-        img.src = "foto-tarde.jpg."
-    } if (hora <= 18 & hora >= 23) { 
+        img.src = "img/foto-tarde.jpg"
+    } if (hora >= 18 && hora <= 23) { 
         // BOA NOITE!
-        img.src = "foto-noite.jpg."
-    } else if (hora >= 1 & hora <= 4) {
+        img.src = "img/foto-noite.jpg"
+    } else if (hora >= 0 && hora <= 4) {
         // BOA NOITE!
-        img.src = "foto-noite.jpg."
+        img.src = "img/foto-noite.jpg"
     }
 }
