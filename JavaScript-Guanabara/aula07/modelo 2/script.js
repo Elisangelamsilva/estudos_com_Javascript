e8986acede36818223c326c2f371c1088b45ecd4
Elisangela <elisangela00silva@gmail.com>
2020-05-28 16:49:24 -0300

    atualização function

--- JavaScript-Guanabara/aula07/modelo 2/script.js
+++ JavaScript-Guanabara/aula07/modelo 2/script.js
@@ -2,4 +2,5 @@ function verificar(){
     var data = new Date()
     var ano = data.getFullYear() // ano atual com 4 casas
     var fano = document.getElementById("txtano")
+    var res = document.getElementById('res')
 }
