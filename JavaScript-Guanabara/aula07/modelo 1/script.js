c8c9642b567afa10c39c88445f75b8a8d8d8f2d6
Elisangela <elisangela00silva@gmail.com>
2020-05-27 22:20:53 -0300

    atualização querySelector

--- JavaScript-Guanabara/aula07/modelo 1/script.js
+++ JavaScript-Guanabara/aula07/modelo 1/script.js
@@ -1,6 +1,6 @@
 function carregar(){
     var msg = window.document.querySelector('div#msg')
-    var img = window.document.querySelector('div#foto')
+    var img = window.document.querySelector('div#img1')
     var data = new Date()
     var hora = data.getHours()
     msg.innerHTML = `Agora são ${hora} horas.`
