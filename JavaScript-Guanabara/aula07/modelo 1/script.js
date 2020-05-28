2e55810178cfcc47285c22810d8d089aa5f6d784
Elisangela <elisangela00silva@gmail.com>
2020-05-27 22:33:00 -0300

    add lógica

--- JavaScript-Guanabara/aula07/modelo 1/script.js
+++ JavaScript-Guanabara/aula07/modelo 1/script.js
@@ -4,7 +4,18 @@ function carregar(){
     var data = new Date()
     var hora = data.getHours()
     msg.innerHTML = `Agora são ${hora} horas.`
+
+    if (hora >= 5 & hora <= 12){
+        // BOM DIA
+        img.src = "foto-manha.jpg."
+    } else if (hora > 12 & hora < 18){
+        // BOA TARDE!
+        img.src = "foto-tarde.jpg."
+    } if (hora <= 18 & hora >= 23) { 
+        // BOA NOITE!
+        img.src = "foto-noite.jpg."
+    } else if (hora >= 1 & hora <= 4) {
+        // BOA NOITE!
+        img.src = "foto-noite.jpg."
+    }
 }
-
-
-
