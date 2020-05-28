f39e8b4a945f671365939a00de5851bf3990bc2d
Elisangela <elisangela00silva@gmail.com>
2020-05-28 14:53:18 -0300

    add cor de fundo

--- JavaScript-Guanabara/aula07/modelo 1/script.js
+++ JavaScript-Guanabara/aula07/modelo 1/script.js
@@ -8,14 +8,18 @@ function carregar(){
     if (hora >= 5 && hora <= 12){
         // BOM DIA
         img.src = "img/foto-manha.jpg"
+        document.body.style.background = ' rgb(211, 212, 153)'
     } else if (hora > 12 && hora < 18){
         // BOA TARDE!
         img.src = "img/foto-tarde.jpg"
+        document.body.style.background = ' rgb(194, 182, 141)'
     } if (hora >= 18 && hora <= 23) { 
         // BOA NOITE!
         img.src = "img/foto-noite.jpg"
+        document.body.style.background = 'green'
     } else if (hora >= 0 && hora <= 4) {
         // BOA NOITE!
         img.src = "img/foto-noite.jpg"
+        document.body.style.background = 'green'
     }
 }
