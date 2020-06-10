0c80639680f111eb989c7a101100e013ce60a78e
Elisangela <elisangela00silva@gmail.com>
2020-06-10 11:41:05 -0300

    informa ao cliente falta de dados

--- JavaScript-Guanabara/aula10/modelo 1 copy 2/script.js
+++ JavaScript-Guanabara/aula10/modelo 1 copy 2/script.js
@@ -2,4 +2,10 @@ function contar(){
     var ini = document.getElementById("txti")
     var fim = document.getElementById("txtf")
     var passo = document.getElementById("txtp")
+
+    if(ini.value.length == 0 || fim.value.length == 0 || passo.value.length){
+        window.alert("[ERRO] Faltam dados!")
+    }else{
+        
+    }
 }
