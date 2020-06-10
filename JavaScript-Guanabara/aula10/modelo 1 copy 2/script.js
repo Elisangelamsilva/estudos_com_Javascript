bee0a8bc39d3c2f64f7d0210ba20c4d79bc28975
Elisangela <elisangela00silva@gmail.com>
2020-06-10 11:43:39 -0300

    converte em number

--- JavaScript-Guanabara/aula10/modelo 1 copy 2/script.js
+++ JavaScript-Guanabara/aula10/modelo 1 copy 2/script.js
@@ -2,10 +2,14 @@ function contar(){
     var ini = document.getElementById("txti")
     var fim = document.getElementById("txtf")
     var passo = document.getElementById("txtp")
+    var res = document.getElementById("res")
 
     if(ini.value.length == 0 || fim.value.length == 0 || passo.value.length){
         window.alert("[ERRO] Faltam dados!")
     }else{
-        
+        res.innerHTML = `Contando: `
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
     }
 }
