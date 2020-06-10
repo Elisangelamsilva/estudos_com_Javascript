bb8e7a892fbd2317ace6a49cdfa3e94c102edf56
Elisangela <elisangela00silva@gmail.com>
2020-06-10 11:48:04 -0300

    atualizção emotion

--- JavaScript-Guanabara/aula10/modelo 1 copy 2/script.js
+++ JavaScript-Guanabara/aula10/modelo 1 copy 2/script.js
@@ -13,7 +13,7 @@ function contar(){
         let p = Number(passo.value)
 
         for(let c = i; c <= f; c+= p){
-            res.innerHTML += `${c}\u{1F603}`
+            res.innerHTML += `${c}\u{1F449}`
         }
     }
 }
