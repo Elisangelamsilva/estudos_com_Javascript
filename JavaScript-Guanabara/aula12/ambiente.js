4e4bfeb62831ebeb92f16a07f9758d19c18492ae
Elisangela <elisangela00silva@gmail.com>
2020-06-11 01:21:01 -0300

    tamanho da array

--- JavaScript-Guanabara/aula12/ambiente.js
+++ JavaScript-Guanabara/aula12/ambiente.js
@@ -4,3 +4,4 @@ console.log(`Nosso vetor é o ${num}`)
 // Acrecentar um valor 
 num[3] = 6 //adicionar o valor 6 no índice 3
 num.push() //adicionar sem indicar índice
+num.length // saber o comprimento, é um atributo
