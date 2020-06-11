6d49eb3625846875d27172e9af55789f01f99a1f
Elisangela <elisangela00silva@gmail.com>
2020-06-11 17:41:44 -0300

    pegar todos os elementos e colocar em ordem crescente

--- JavaScript-Guanabara/aula12/ambiente.js
+++ JavaScript-Guanabara/aula12/ambiente.js
@@ -1,8 +1,23 @@
 let num = [5, 8, 9, 3]
 
 console.log(`Nosso vetor é o ${num}`)
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 // Acrecentar um valor 
 num[3] = 6 //adicionar o valor 6 no índice 3
 num.push() //adicionar sem indicar índice
 num.length // saber o comprimento, é um atributo
 num.sort() //pegar todos os elementos e colocar em ordem crescente
+*/
