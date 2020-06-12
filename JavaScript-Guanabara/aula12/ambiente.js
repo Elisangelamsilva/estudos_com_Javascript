a197e024906914af07ed3411974929903104935f
Elisangela <elisangela00silva@gmail.com>
2020-06-12 11:40:31 -0300

    exemplo de indexOf

--- JavaScript-Guanabara/aula12/ambiente.js
+++ JavaScript-Guanabara/aula12/ambiente.js
@@ -5,6 +5,9 @@ num.sort()
 console.log(num)
 console.log(`Nosso vetor é o ${num} e tem ${num.length} posições.`)
 console.log(`O primeiro valor do vetor é ${num[0]}`)
+let pos = num.indexOf(8)
+console.log(`O valor 8 está na posição ${pos}`)
+
 
 /*
 // Acrecentar um valor 
