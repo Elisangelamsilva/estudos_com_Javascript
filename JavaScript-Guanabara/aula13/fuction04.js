afbdf34a7e52b2a52f75568a04cafe713a18312f
Elisangela <elisangela00silva@gmail.com>
2020-06-12 12:46:31 -0300

    recursividade

--- JavaScript-Guanabara/aula13/fuction04.js
+++ JavaScript-Guanabara/aula13/fuction04.js
@@ -1,9 +1,19 @@
 // 5! = 5 x 4 x .... 
 function fatorial(n){
     let fat = 1
-    for(let c = n; c > 1; c++){
+    for(let c = n; c > 1; c--){
         fat *= c
     }
     return fat
 }
 console.log(fatorial(5))
+
+// outro jeito de fazer 
+
+function fatori(n){
+    if(n ===1){
+        return 1
+    }else{
+        return n * fatori(n-1)
+    }
+}
