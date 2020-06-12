c89e9b829cd9746fa1d9f0152e30fadda2127219
Elisangela <elisangela00silva@gmail.com>
2020-06-12 18:10:23 -0300

    lógica function finalização

--- JavaScript-Guanabara/aula14/script.js
+++ JavaScript-Guanabara/aula14/script.js
@@ -26,6 +26,7 @@ function calcular(){
         let item = documente.createElement('option')
         item.text = `Valor ${recebe.value} adicionado`
         lista.appendChild(item)
+        res.innerHTML = ``
     }else{
         window.alert("Por favor, digite um número válido ou já está em lista!")
     }
@@ -40,8 +41,29 @@ function finalizar(){
         window.alert("Adicione valores!")
     } else{
         let tot = valores.length
+        let maior = valores[0]
+        let menor = valores[0]
+        let soma = 0
+        let media = 0
+
+
+        for(let pos in valores){
+            soma += valores[pos]
+            if(valor[pos] > maior)
+                maior = valores[pos]
+            if(valores[pos] < menor)
+                menor = valores[pos]
+        }
+
+        media = soma / tot
+
         res.innerHTML = ``
         res.innerHTML += `<p> Ao todo temos ${tot} números cadastrados.</p>`
+        res.innerHTML += `<p>O menor valor informado ${menor}</p>`
+        res.innerHTML += `<p>O maior valor informado foi ${maior}</p>`
+        res.innerHTML += `<p>Somando todos os valores, temos ${soma}. </p>`
+        es.innerHTML += `<p>A média dos valores digitados é ${media}.</p>`
+        es.innerHTML += `<p></p>`
     }
 
 }
