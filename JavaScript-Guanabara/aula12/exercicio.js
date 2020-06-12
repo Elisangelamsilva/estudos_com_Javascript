5f5755b6351e8e29a2aef8406df9771cc6b70929
Elisangela <elisangela00silva@gmail.com>
2020-06-12 11:35:38 -0300

    for mais simplificado

--- JavaScript-Guanabara/aula12/exercicio.js
+++ JavaScript-Guanabara/aula12/exercicio.js
@@ -2,3 +2,9 @@ let num = [5, 8, 9, 3, 7, 3, 5]
 for(var i = 0; i < num.length; i++){
     console.log(`A posição ${i} tem o valor ${num[i]}`)
 }
+
+// outro modo de fazer - simplificado - só funciona para array e object
+
+ for(var i in num){
+     console.log(`A posição ${i} tem o valor ${num[i]}`)
+ }
