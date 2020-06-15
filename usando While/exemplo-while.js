add951c4088bc766bc32d6d1bfe1bf931de949a1
Elisangela <elisangela00silva@gmail.com>
2020-06-15 20:07:36 -0300

    função que gera numeros aleatorios

---
+++ usando While/exemplo-while.js
@@ -0,0 +1,18 @@
+// while classico
+
+// exemplo 01
+let i = 0
+
+while(i <= 10){
+    console.log(i)
+    i++
+}
+
+// exemplo 02
+const nome = 'Maria'
+let a = 0
+
+while(a < nome.length){
+    console.log(nome[i])
+    a++
+}
