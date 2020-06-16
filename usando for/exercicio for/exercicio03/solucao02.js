585acc25f88e7ea878af7c07e5d06d0f0d799f8c
Elisangela <elisangela00silva@gmail.com>
2020-06-16 14:22:13 -0300

    inicio lógica solução 10

--- usando for/exercicio for/exercicio03/solucao02.js
+++ usando for/exercicio for/exercicio03/solucao02.js
@@ -20,3 +20,28 @@ function solucao07(){
     tagCreat.innerHTML += ` é: ${media01}`
     solucao07.appendChild(tagCreat)
 }
+
+function solucao10(){
+    let noUm = Number(document.getElementById("num01").value)
+    let noDois = Number(document.getElementById("num02").value)
+    let noTres = Number(document.getElementById("num03").value)
+    let codiAlunos = Number(document.getElementById("codigo02").value)
+    let solucao10 = document.querySelector("#solucao10")
+    let soma02 = 0
+    let media02 = 0
+
+    if(codioAlunos <= 0){
+        alert("Insira um número válido!")
+    }
+    if(){
+
+    }
+    
+    soma01 = notaUm + notaDois + notaTres
+    media01 = soma01 / 3
+
+
+
+
+
+}
