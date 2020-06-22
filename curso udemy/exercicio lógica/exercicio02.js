08d6cedf4610efa49da3351bc99cbecb7a8281e1
Elisangela <elisangela00silva@gmail.com>
2020-06-22 11:08:47 -0300

    add exercicio 02 e 03

--- curso udemy/exercicio lógica/exercicio02.js
+++ curso udemy/exercicio lógica/exercicio02.js
@@ -2,3 +2,16 @@
  * Escreva uma função chamada ePaisagem que recebe dois argumentos, largura e altura de uma imagem (number).
  * Retorne true se a imagem estiver no modo paisagem.
  */
+
+ function ePaisagem(largura, altura){
+     return largura > altura ? true : false;
+     // já vai retorna por isso poderia usar return largura > altura;
+ }
+
+ console.log(ePaisagem(1920, 1080))
+
+ // converter para array function
+
+ const ePaisagem = (largura, altura) => largura > altura;
+
+console.log(ePaisagem(1920, 1080))
---
+++ curso udemy/exercicio lógica/exercicio03.js
@@ -0,0 +1,20 @@
+// Escreva uma função que recebe um número e retorne o seguinte:
+// Número é divisível por 3 = Fizz
+// Número é divisível por 5 = Buzz
+// Número NÃO é divisível por 3 e 5 = Retorna o próprio número 
+// Checar se o número é realmente um número
+// Use a função com números de 0 a 100
+
+function fizzBuzz(numero) {
+    if(typeof numero !== "number") return numero;
+    if (numero % 3 === 0 && numero % 5 === 0) return "FizzBuzz"; //deve ser checada primeiro
+    if (numero % 3 === 0) return "Fizz";
+    if (numero % 5 === 0) return "Buzz";
+    if (numero % 5 === 0) return "Buzz";
+    return 
+}
+ 
+// testar a function
+for(let i = 0; i <= 100; i++){
+    console.log(i, fizzBuzz(i))
+}
