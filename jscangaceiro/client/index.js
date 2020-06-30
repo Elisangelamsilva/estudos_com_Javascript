72e144adeb63153dff8c328013e044a97710489d
Elisangela <elisangela00silva@gmail.com>
2020-06-30 13:53:10 -0300

    array campos que armazena cada entrada do formulário

---
+++ jscangaceiro/client/index.js
@@ -0,0 +1,7 @@
+// array campos que armazena cada entrada do formulário. API do DOM que nos permite buscar elementos através de seletores CSS.
+
+var campos = [
+    document.querySelector('#data'),
+    document.querySelector('#valor'),
+    document.querySelector('#quantidade')
+];
