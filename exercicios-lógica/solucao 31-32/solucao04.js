75594fb0d5a9606d8b573ae576bf38d9dffea023
Elisangela <elisangela00silva@gmail.com>
2020-07-01 20:36:08 -0300

    finalização solução 36 e meio termo 34 - 35

--- exercicios-lógica/solucao 31-32/solucao04.js
+++ exercicios-lógica/solucao 31-32/solucao04.js
@@ -56,3 +56,90 @@ function solucao33(){
         solucao33.innerHTML += `<p>${numero03}x${i} = ${num}</p>`
     }
 }
+
+function solucao34(){
+    let solucao34 = document.getElementById("solucao34")
+    let fromContainer = Array.from(document.querySelectorAll(".form-container"))
+    let alto = 0
+    let baixo = 0
+    let alunoBaixo = 0
+    let alunoAlto = 0
+    console.log(fromContainer)
+
+    for(let dados of fromContainer){
+        let alunosCodigos = dados.querySelectorAll(".codigo")
+        let alunosAlturas = dados.querySelectorAll(".altura")
+        console.log(alunosAlturas)
+        console.log(alunosCodigos)
+
+        if(alunosAlturas > alto){
+            alto = alto
+            alunoAlto = alunosCodigos
+        }if(alunosAlturas < baixo){
+            baixo = baixo
+            alunoBaixo = alunoBaixo
+
+
+        }
+        console.log(alunoAlto, alto)
+        console.log(alunoBaixo, baixo)
+        
+    }
+
+}
+
+
+function solucao35(){
+    let solucao35 = document.getElementById("solucao35")
+    let fromContainer = Array.from(document.querySelectorAll(".form-container"))
+    let arrayPessoa = []
+    let mediaMulheres = 0
+    let maiorAltura = 0
+    let menorAltura = 0
+
+
+    for(let dados of fromContainer){
+        let sexo = k.querySelector('select[name="sexo"]').value
+        let altura = Number(k.querySelector('input[name="altura"]').value)
+    
+        let pessoa = {
+            sexo: sexo,
+            altura: altura,
+        }
+        arrayPessoa.push(pessoa);
+    }
+
+    for(let pessoa of arrayPessoa){
+
+    }
+
+}
+
+
+function solucao36(){
+    let numeros = document.querySelector("#numero06")
+    let numeroNumber = Number(numeros.value)
+
+        if(!window.lista){
+            window.lista = []
+        }
+        window.lista.push(numeroNumber)
+
+        numeros.value = ``
+        numeros.focus( )
+}
+function finalizar01(){
+    let solucao36 = document.querySelector("#solucao36")
+    let resultado = 1
+
+    for(dados of window.lista){
+        let resultado = 1
+        for(var b = dados; b > 1; b--){
+            resultado = resultado * b;
+        }
+        solucao36.innerHTML += `<p>${dados}! = ${resultado} <br></p>`    
+    }
+
+}
+
+
