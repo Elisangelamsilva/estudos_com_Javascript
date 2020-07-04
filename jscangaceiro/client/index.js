5771e45d599edbdf258730b65be755ee1ead0cc0
Elisangela <elisangela00silva@gmail.com>
2020-07-04 14:16:58 -0300

    inicio da criação do elemento <tr>

--- jscangaceiro/client/index.js
+++ jscangaceiro/client/index.js
@@ -9,5 +9,5 @@ var campos = [
 var tbody = document.querySelector('table tbody');
 
 document.querySelector('.form').addEventListener('submit', function(event) {
-alert('oi');
+    var tr = document.createElement('tr');
 });
