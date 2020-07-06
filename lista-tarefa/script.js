0e086e9097c9bb97fcf9890e0ad402bbe598d698
Elisangela <elisangela00silva@gmail.com>
2020-07-06 14:10:25 -0300

    já é possivel add tarefas na lista

--- lista-tarefa/script.js
+++ lista-tarefa/script.js
@@ -2,8 +2,25 @@ const inputTarefa = document.querySelector(".input-tarefa")
 const btnTarefa = document.querySelector(".btn-tarefa")
 const tarefa = document.querySelector(".tarefa")
 
+
+
 // capturar o event click
 btnTarefa.addEventListener("click", function(){
     // quando clicar e não tiver nada
     if(!inputTarefa.value) return;
+    // capturar o texto e jogar em outra função
+    criaTarefa(inputTarefa.value)
 })
+
+
+function criaTarefa(textoInput){
+    const li = criaLi();
+    li.innerText = textoInput;
+    tarefa.appendChild(li);
+}
+
+//criar uma tarefa
+function criaLi(){ 
+    const li = document.createElement("li");
+    return li;
+}
