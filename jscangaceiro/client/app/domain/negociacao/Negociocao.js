5be723b8a3d6ea3b0d3adac2283dac3f5be36da0
Elisangela <elisangela00silva@gmail.com>
2020-07-19 14:07:57 -0300

    constructor de classe

--- jscangaceiro/client/app/domain/negociacao/Negociocao.js
+++ jscangaceiro/client/app/domain/negociacao/Negociocao.js
@@ -1,8 +1,8 @@
 class Negociacao{
     //definição das propriedades da classe
     constructor(data,quantidade,valor){
-        this.data = new Data(); //data atual
-        this.quantidade = 1;
-        this.valor = 0.0;
+        this.data = data; //data atual
+        this.quantidade = quantidade;
+        this.valor = valor;
     }
 }
