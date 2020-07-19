ab018911d41d3a6152a05ddbb359915b398a0bc0
Elisangela <elisangela00silva@gmail.com>
2020-07-19 12:31:56 -0300

    classe negociação

---
+++ jscangaceiro/client/app/domain/negociacao/Negociocao.js
@@ -0,0 +1,8 @@
+class Negociacao{
+    //definição das propriedades da classe
+    constructor(){
+        this.data = new Data(); //data atual
+        this.quantidade = 1;
+        this.valor = 0.0;
+    }
+}
