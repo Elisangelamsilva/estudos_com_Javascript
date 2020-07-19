0daeb091c76895708c4f1efae6d2f7481a34f863
Elisangela <elisangela00silva@gmail.com>
2020-07-19 13:58:52 -0300

    constructor receber os dados atraves de parametros

--- jscangaceiro/client/app/domain/negociacao/Negociocao.js
+++ jscangaceiro/client/app/domain/negociacao/Negociocao.js
@@ -1,6 +1,6 @@
 class Negociacao{
     //definição das propriedades da classe
-    constructor(){
+    constructor(data,quantidade,valor){
         this.data = new Data(); //data atual
         this.quantidade = 1;
         this.valor = 0.0;
