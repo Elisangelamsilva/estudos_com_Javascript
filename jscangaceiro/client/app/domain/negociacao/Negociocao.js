66236d0a78b36edbbd4ea0eb43a7f8507672f784
Elisangela <elisangela00silva@gmail.com>
2020-07-19 14:34:35 -0300

    encapsulamento

--- jscangaceiro/client/app/domain/negociacao/Negociocao.js
+++ jscangaceiro/client/app/domain/negociacao/Negociocao.js
@@ -1,12 +1,24 @@
 class Negociacao{
     //definição das propriedades da classe
     constructor(data,quantidade,valor){
-        this.data = data; //data atual
-        this.quantidade = quantidade;
-        this.valor = valor;
+        this._data = data; //data atual
+        this._quantidade = quantidade;
+        this._valor = valor;
     }
 
     obtemVolume(){
-        return this.quantidade * this.valor;
+        return this._quantidade * this._valor;
+    }
+
+    getData() {
+        return this._data;
+    }
+
+    getQuantidade() {
+        return this._quantidade;
+        }
+        
+    getValor() {
+        return this._valor;
     }
 }
