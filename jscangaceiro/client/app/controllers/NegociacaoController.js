302c032534d7d4f9664829d884f69666f1de8a5e
Elisangela <elisangela00silva@gmail.com>
2020-07-20 17:48:15 -0300

    criando uma instancia de  negociação

--- jscangaceiro/client/app/controllers/NegociacaoController.js
+++ jscangaceiro/client/app/controllers/NegociacaoController.js
@@ -3,7 +3,7 @@ class NegociacaoController{
     //Sendo assim, a busca pelos elementos do DOM só serão realizadas uma única vez no constructor, e não mais a cada chamada do método adicionar().
     constructor() {
         // buscando os elementos
-        this._inputData = document.querySelector('#data');
+        this._inputData = document.querySelector('#data').value;
         this._inputQuantidade = Number(document.querySelector('#quantidade').value);
         this._inputValor = Number(document.querySelector("#valor").value);
 
@@ -13,6 +13,10 @@ class NegociacaoController{
     adiciona(event) {
         // cancelando a submissão do formulário
         event.preventDefault();
+
+        let negociacao = new Negociacao(
+            this._inputData.value,this._inputQuantidade,this._inputValor
+        )
 
         }
 }
