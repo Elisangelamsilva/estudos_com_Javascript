c8cfeefa0deb518794cda3a781e2805c06a4b7e3
Elisangela <elisangela00silva@gmail.com>
2020-07-20 14:21:18 -0300

    utilizando Object.assign

--- jscangaceiro/client/app/domain/negociacao/Negociocao.js
+++ jscangaceiro/client/app/domain/negociacao/Negociocao.js
@@ -1,8 +1,8 @@
 class Negociacao{
     //definição das propriedades da classe
-    constructor(data,quantidade,valor){
-        Object.assign(this, { _data: new Date(data.getTime()), _quantidade: quantidade, _valor: valor});
-
+    constructor(_data,_quantidade,_valor){
+        Object.assign(this, { _quantidade, _valor });
+        this._data = new Date(_data.getTime());
         Object.freeze(this);
     }
 
