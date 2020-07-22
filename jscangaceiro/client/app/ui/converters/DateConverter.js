55b4a576157775a47e0dcba025b8618ecf8d51ba
Elisangela <elisangela00silva@gmail.com>
2020-07-22 12:09:37 -0300

    fail-fast e expressões regulares

--- jscangaceiro/client/app/ui/converters/DateConverter.js
+++ jscangaceiro/client/app/ui/converters/DateConverter.js
@@ -1,13 +1,15 @@
 class DateConverter{
-   static paraTexto(data) {
-        return data.getDate()
-            + '/' + (data.getMonth() + 1)
-            + '/' + data.getFullYear();
+    static paraTexto(data) {
+           return `${data.getDate()}/${data.getMonth()+1}/${data.get
+    FullYear()}`;
     }
 
-    paraData(texto) {
-        return `${data.getDate()}/${data.getMonth()+1}/${data.get
-    FullYear()}`;
+    static paraData(texto) {
+        //só será executada se o if for false
+        if(!/^\d{4}-\d{2}-\d{2}$/.test(texto)) {
+            throw new Error('Deve estar no formato aaaa-mm-dd');
+        }
+        return new Date(...texto.split('-').map((item,indice) => item - indice % 2));
     }
 
     constructor() {
